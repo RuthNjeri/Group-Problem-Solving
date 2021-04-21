c3123815b982d9ccc02fe733a894ae699e5c7ed9
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-04-21 21:15:11 +0300

    remove unnecessary brackets in python code

--- Arrays/non_constructible_change.py
+++ Arrays/non_constructible_change.py
@@ -4,12 +4,12 @@ def nonConstructibleChange(coins):
 
   for coin in coins:
     if coin > currentChangeCreated + 1:
-      return (currentChangeCreated + 1)
+      return currentChangeCreated + 1
 
     currentChangeCreated += coin
 
 
-  return (currentChangeCreated + 1)
+  return currentChangeCreated + 1
 
 # Test Cases
 nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]) # 20
