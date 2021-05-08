a7b5bff786ced831fb23b224d839f023250ec505
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-05-08 15:33:35 +0300

    #10 add Ruby solution

--- Strings/ValidPalindrome.py
+++ Strings/ValidPalindrome.py
@@ -12,3 +12,30 @@ class Solution:
         
         # check if string is equal to its reverse
         return s == s[::-1]
+
+# In Ruby
+# First remove any character that is not alphanumeric.
+# Then use two pointers
+# one at the start and another at the end(stop)
+# if at any point the start character is not equal to the stop character, return false
+
+
+# def is_palindrome(s)
+
+#   valid_string = ""
+
+#   for char in 0...s.length
+#       if s[char].match?(/[A-Za-z0-9]/)
+#           valid_string << s[char].downcase
+#       end
+#   end
+
+#   start = 0
+#   stop = valid_string.length - 1
+#   while start < stop
+#       return false if valid_string[start] != valid_string[stop]
+#       start += 1
+#       stop -= 1
+#   end
+#   true
+# end
