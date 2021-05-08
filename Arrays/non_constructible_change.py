c722ae0c008f122eddb463b6a44a1e759e1cedc0
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-05-08 11:37:26 +0300

    add explanation to non-constructible change problem

--- Arrays/non_constructible_change.py
+++ Arrays/non_constructible_change.py
@@ -1,3 +1,16 @@
+# The idea is that if you order the coins,
+# you will always get the minimum number of possible
+# coins you can create as you add them up
+# if the next coin is larger than the current_sum + 1
+# then the coin you cannot create is the current_sum + 1
+# Example, with [1, 2, 4]
+# You can creat 1, 2, 3, 4, 5, 6, 7 because the total so far is 7 and at each point
+# the coin was not greater than the total + 1
+# total = 0 current_coin = 1, current_coin > total + 1 = false
+# total = 1 current_coin = 2, current_coin > total + 1 = false
+# total = 3 current_coin = 4, current_coin > total + 1 = false
+# return total + 1 which is 8, you cannot create 8 from 4, 2, and 1 
+
 def nonConstructibleChange(coins):
   coins.sort()
   currentChangeCreated = 0
