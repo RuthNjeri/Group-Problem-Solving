2258cc10a696dbc338eb7524aa85162c44d0d2b4
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-06-01 15:40:55 +0300

    #32 unique paths

---
+++ Dynamic/unique_paths.rb
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/unique-paths/
+def unique_paths(m, n)
+  grid = Array.new(m){Array.new(n - 1)}
+
+  # The paths on the top row are 1s
+  # The cells on the right are 1s
+  # For optimization, this can be done inside the double
+  # for loop..if col == 0 the cell is 1, and or if row == 0 the cell is 1
+  for col in 0...n
+      grid[0][col] = 1
+  end
+
+  for row in 1...m
+      for col in 0...n
+          up = row - 1
+          left = col - 1
+          if grid[up][col] != nil && grid[row][left] != nil
+              grid[row][col] = grid[up][col] + grid[row][left]
+          else
+              grid[row][col] = grid[up][col] if grid[up][col] != nil
+              grid[row][col] = grid[row][left]if grid[row][left] != nil
+          end
+      end
+  end
+  grid[m - 1][n - 1]
+end
